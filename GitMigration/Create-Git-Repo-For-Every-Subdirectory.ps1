6a8e7526dd1717d97e2d74399159648eed4a494c
David Whitney <david.whitney@euromoneyplc.com>
2015-03-30 15:45:14 +0100

    generate packaging script.

--- GitMigration/Create-Git-Repo-For-Every-Subdirectory.ps1
+++ GitMigration/Create-Git-Repo-For-Every-Subdirectory.ps1
@@ -5,7 +5,7 @@ $scriptHome = Split-Path $invocation.MyCommand.Path
 
 $path = "C:\dev\svn\Solutions\Class Libraries";
 $pathSpec = $path + "\*";
-$targetPath = "c:\dev\git-svn\Class Libraries";
+$targetPath = "C:\dev\git-svn\Class Libraries";
 
 function CopyFiles($target, $destination) 
 {
@@ -34,10 +34,17 @@ function CreateNuSpec($destination)
 	Get-ChildItem -Path $destination -Filter "*proj" -Recurse | ForEach-Object { 
 		$pathToProj = $_.FullName.Replace($_.Name, "");
 		$nameStem = $_.Name.Replace(".csproj", "").Replace(".vbproj", "");
-
+		$extension = $_.Name.Replace("$nameStem", "");
+		$relativePath = $pathToProj.Replace($destination + "\", "");
+		
 		xcopy "$scriptHome\template.nuspec" "$pathToProj" /Y /F
 		Rename-Item "$pathToProj\template.nuspec" "$nameStem.nuspec"
-	
+		
+		$packPath = "$relativePath$nameStem$extension";
+		$packOpts = "-OutputDirectory artifacts -Build -IncludeReferencedProjects -NonInteractive"
+		
+		Add-Content "$destination\package.cmd" ".nuget\nuget pack $packPath $packOpts"
+
 		echo "Created $pathToProj"
 	}
 }
