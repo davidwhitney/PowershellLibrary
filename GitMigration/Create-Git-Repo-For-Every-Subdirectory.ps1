8f8fbc82be9b1bfe452c8a10de104e4a5ddd7522
David Whitney <david.whitney@euromoneyplc.com>
2015-03-30 14:33:56 +0100

    Hacking scripts.

--- GitMigration/Create-Git-Repo-For-Every-Subdirectory.ps1
+++ GitMigration/Create-Git-Repo-For-Every-Subdirectory.ps1
@@ -1,19 +1,35 @@
 # Get-ChildItem -Recurse | ?{ $_.PSIsContainer } | ForEach-Object {   
 
-Get-ChildItem "C:\dev\svn\Solutions\Windows Applications\*" | ForEach-Object { 
+$path = "C:\dev\svn\Solutions\Class Libraries";
+$pathSpec = $path + "\*";
+$targetPath = "c:\dev\git-svn\Class Libraries";
+
+function CopyFiles($target, $destination) 
+{
+	New-Item $newPath -ItemType Directory -ErrorAction SilentlyContinue; 
+	robocopy $target $destination /E
+}
+
+function CreateGitRepository($repoName, $directory)
+{
+	$gitRepoName = $repoName.Replace(" ", "-");
+		
+	cd $newPath
+	git init
+	git add .
+	git commit -m "Import"
+	git remote add origin ssh://git@stash.euromoneydigital.com:7999/winapps/$gitRepoName.git
+
+}
+
+Get-ChildItem $pathSpec | ForEach-Object { 
 	if ($_.FullName -ne "git-migrate.ps1"){
 	
-		$newPath = $_.FullName.Replace("\svn\Solutions","\git-svn");
-		New-Item $newPath -ItemType Directory -ErrorAction SilentlyContinue;   
+		$newPath = $_.FullName.Replace($path, $targetPath);  
 	
-		$gitRepoName = $_.Name.Replace(" ", "-");
-	
-		robocopy $_ $newPath /E
-		cd $newPath
-		git init
-		git add .
-		git commit -m "Import"
-		git remote add origin ssh://git@stash.euromoneydigital.com:7999/winapps/$gitRepoName.git
+		CopyFiles($_, $newPath);
+		CreateNuSpec($newPath);
+		CreateGitRepository($_.Name, $newPath);
+		
 	}
-	
 }
